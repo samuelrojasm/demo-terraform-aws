b3521830a63f712807aab88f0c57a054b3bd06fe
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-03-31 20:09:11 -0600

    ec2-same-family

---
+++ EC2/ec2-same-family/outputs.tf
@@ -0,0 +1,18 @@
+# Definición de valores de salida, para ver
+# los detalles de la creación de las instancias EC2
+
+output "ec2_info" {
+    description = "Descripción de las instancias EC2 creadas"
+    value = [
+        for instance in aws_instance.ec2_family : {
+            instance_id   = instance.id
+            instance_type = instance.instance_type
+            ami_id = instance.ami
+            public_ip     = instance.public_ip
+            private_ip        = instance.private_ip
+            availability_zone = instance.availability_zone
+            tags = instance.tags
+            state = instance.state
+        }
+    ]
+}
