e28cba1737cd364c7a252652d13dbcb9b3001704
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-03-31 20:45:36 -0600

    ec2-same-family

--- EC2/ec2-same-family/outputs.tf
+++ EC2/ec2-same-family/outputs.tf
@@ -5,16 +5,18 @@ output "ec2_info" {
   description = "Descripción de las instancias EC2 creadas"
   value = [
     for instance in aws_instance.ec2_family : {
-      instance_id       = instance.id
-      instance_arn      = instance.arn
-      instance_type     = instance.instance_type
-      ami_id            = instance.ami
-      public_ip         = instance.public_ip
-      private_ip        = instance.private_ip
-      availability_zone = instance.availability_zone
-      private_dns       = instance.private_dns
-      tags              = instance.tags
-      state             = instance.instance_state
+      instance_id            = instance.id
+      instance_arn           = instance.arn
+      instance_type          = instance.instance_type
+      ami_id                 = instance.ami
+      public_ip              = instance.public_ip
+      private_ip             = instance.private_ip
+      availability_zone      = instance.availability_zone
+      private_dns            = instance.private_dns
+      tags                   = instance.tags
+      ebs_block_device       = instance.ebs_block_device
+      ephemeral_block_device = instance.ephemeral_block_device
+      state                  = instance.instance_state
     }
   ]
 }
