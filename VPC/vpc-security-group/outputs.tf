a886017b9cbab3473101a2f4c2834cfe86545f95
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-04-03 14:58:01 -0600

    vpc-security-group

--- VPC/vpc-security-group/outputs.tf
+++ VPC/vpc-security-group/outputs.tf
@@ -1,4 +1,19 @@
-output "security_group_ids" {
-  description = "IDs de los Security Groups creados"
-  value       = { for k, sg in aws_security_group.sgs : k => sg.id }
+output "security_groups" {
+  value = {
+    "web" = {
+      id          = aws_security_group.sg-web.id
+      arn         = aws_security_group.sg-web.arn
+      name        = aws_security_group.sg-web.name
+      description = aws_security_group.sg-web.description
+      vpc_id      = aws_security_group.sg-web.vpc_id
+    }
+    "db" = {
+      id          = aws_security_group.sg-database.id
+      arn         = aws_security_group.sg-database.arn
+      name        = aws_security_group.sg-database.name
+      description = aws_security_group.sg-database.description
+      vpc_id      = aws_security_group.sg-database.vpc_id
+    }
+  }
 }
+
