beaa8a02aa23bdfbc93334c364b2bcba2ba104ef
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-05-02 20:12:53 -0600

    módulo vpc-basic

--- modules/vpc/vpc-basic/outputs.tf
+++ modules/vpc/vpc-basic/outputs.tf
@@ -1,9 +1,19 @@
+# Exponer 
+
 output "vpc_id" {
   description = "ID de la VPC"
   value       = aws_vpc.this.id
 }
 
-output "public_subnet_ids" {
-  description = "IDs de subnets públicas"
+output "vpc_name" {
+  value = aws_vpc.this.tags["Name"]
+}
+
+output "subnet_ids" {
+  description = "IDs de subnets"
   value       = aws_subnet.subnets[*].id
+}
+
+output "subnets_ids" {
+  value = [for subnet in aws_subnet.subnets : subnet.id]
 }
