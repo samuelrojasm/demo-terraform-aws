419d130efa5fa7e86cc98ade078c0ae08ebd1760
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-05-30 13:17:05 -0600

    tf-iam-openid-github

---
+++ modules/iam/iam-openid-connect-github/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.11.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configuración del proveedor AWS
+provider "aws" {
+  region  = var.aws_region # Región de AWS donde se crean los recursos
+  profile = "tf"           # Nombre del perfil configurado en AWS CLI con IAM Identity Center
+}
