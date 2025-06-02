601b99d2ddb4504423cfeaadb7c73edbfe12597a
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-06-02 15:01:44 -0600

    tf-iam-oidc-github-role

--- modules/iam/iam-oidc-github-role/outputs.tf
+++ modules/iam/iam-oidc-github-role/outputs.tf
@@ -1,4 +1,20 @@
-output "role_arn" {
-  value       = aws_iam_role.this.arn
-  description = "ARN of the IAM role"
+output "role_info" {
+  description = "Información detallada del IAM Role"
+  value = {
+    name        = aws_iam_role.this.name
+    arn         = aws_iam_role.this.arn
+    id          = aws_iam_role.this.id
+    path        = aws_iam_role.this.path
+    unique_id   = aws_iam_role.this.unique_id
+    create_date = aws_iam_role.this.create_date
+  }
+}
+
+output "policy_info" {
+  description = "Details of the IAM policy"
+  value = {
+    name = aws_iam_policy.this.name
+    arn  = aws_iam_policy.this.arn
+    id   = aws_iam_policy.this.id
+  }
 }
