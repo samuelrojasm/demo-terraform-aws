aaf0cfe3703e608a1114373c503e179edfe1a430
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-06-02 14:54:56 -0600

    tf-ejemplo-oidc-github

--- modules/iam/iam-oidc-github-role/ejemplos/oidc-role-s3-policy/variables.tf
+++ modules/iam/iam-oidc-github-role/ejemplos/oidc-role-s3-policy/variables.tf
@@ -1,8 +1,3 @@
-variable "role_name" {
-  description = "Nombre base del rol"
-  type        = string
-}
-
 variable "s3_bucket_name" {
   description = "Nombre del bucket S3"
   type        = string
