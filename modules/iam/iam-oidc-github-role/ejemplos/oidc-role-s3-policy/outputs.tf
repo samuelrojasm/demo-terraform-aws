0aa8544081582c4bc0f8df8f34b265b144f6fb18
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-06-02 13:17:29 -0600

    tf-ejemplo-oidc-github

--- modules/iam/iam-oidc-github-role/ejemplos/oidc-role-s3-policy/outputs.tf
+++ modules/iam/iam-oidc-github-role/ejemplos/oidc-role-s3-policy/outputs.tf
@@ -1,4 +1,4 @@
 output "role_arn" {
   description = "ARN of the IAM role created"
-  value       = aws_iam_role.github_actions_policy.arn
+  value       = aws_iam_policy.github_actions_policy.arn
 }
