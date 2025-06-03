4cd2550792eeb7bacf9ababa08c4b1eff87396c4
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-06-02 18:44:35 -0600

    tf-ejemplo-oidc-github

--- modules/iam/iam-oidc-github-role/ejemplos/oidc-role-s3-policy/main.tf
+++ modules/iam/iam-oidc-github-role/ejemplos/oidc-role-s3-policy/main.tf
@@ -11,9 +11,11 @@ locals {
 module "github_oidc_role" {
   source         = "../../"
   role_name      = "github-actions-demo-role"
-  aws_account_id = "592660788982"
+  aws_account_id = "1234567890"
   repo_owner     = "miusuario"
   repo_name      = "demo-gitops-repo"
   repo_branch    = "main"
   policy_json    = local.github_actions_policy_json
+  # policy_json    = null
+  # policy_arn_list = ["arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess","arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"]
 }
