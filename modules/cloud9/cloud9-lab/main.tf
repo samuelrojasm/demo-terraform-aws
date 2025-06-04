fbaafd3b3b6e1679cae9afe1676e8c6c1dbe8f87
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-06-04 14:22:28 -0600

    aws-cloud9-module

--- modules/cloud9/cloud9-lab/main.tf
+++ modules/cloud9/cloud9-lab/main.tf
@@ -4,7 +4,7 @@ resource "aws_cloud9_environment_ec2" "this" {
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   automatic_stop_time_minutes = var.automatic_stop_time_minutes
-  image_id = var.image_id
-  connection_type = var.connection_type
-  tags = var.tags
+  image_id                    = var.image_id
+  connection_type             = var.connection_type
+  tags                        = var.tags
 }
--- modules/cloud9/cloud9-lab/variables.tf
+++ modules/cloud9/cloud9-lab/variables.tf
@@ -1,44 +1,44 @@
 variable "name" {
-  type        = string
   description = "Name of the Cloud9 environment"
+  type        = string
 }
 
 variable "description" {
-  type        = string
-  default     = "Cloud9 environment for labs"
+  type    = string
+  default = "Cloud9 environment for labs"
 }
 
 variable "instance_type" {
+  description = "Instance type for Cloud9"
   type        = string
   default     = "t3.small"
-  description = "Instance type for Cloud9"
 }
 
 variable "image_id" {
+  description = "The identifier for the Amazon Machine Image (AMI) that's used to create the EC2 instance"
   type        = string
   default     = "amazonlinux-2023-x86_64"
-  description = "The identifier for the Amazon Machine Image (AMI) that's used to create the EC2 instance"
 }
 
 variable "subnet_id" {
-  type        = string
   description = "Subnet ID where Cloud9 will be deployed"
+  type        = string
 }
 
 variable "automatic_stop_time_minutes" {
+  description = "Time in minutes to automatically stop the instance"
   type        = number
   default     = 60
-  description = "Time in minutes to automatically stop the instance"
 }
 
 variable "connection_type" {
-    type = string
-    default = "CONNECT_SSM"
-    description = "The connection type used for connecting to an Amazon EC2 environment"
+  description = "The connection type used for connecting to an Amazon EC2 environment"
+  type        = string
+  default     = "CONNECT_SSM"
 }
 
 variable "tags" {
+  description = "Tags to apply to the Cloud9 environment"
   type        = map(string)
   default     = {}
-  description = "Tags to apply to the Cloud9 environment"
 }
