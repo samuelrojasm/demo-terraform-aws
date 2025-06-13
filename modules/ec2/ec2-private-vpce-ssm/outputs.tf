6476b1c8cdfee5464e35c63377eb9744e8613dab
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-06-12 23:38:24 -0600

    ec2-private-vpce-ssm

--- modules/ec2/ec2-private-vpce-ssm/outputs.tf
+++ modules/ec2/ec2-private-vpce-ssm/outputs.tf
@@ -6,6 +6,6 @@ output "sg_rules_ec2_egress_rule_description" {
   value = "EC2 tiene salida permitida hacia VPCE por HTTPS (443)"
 }
 
-output "aws_instance:info" {
+output "aws_instance_info" {
   value = module.ec2_ssm.aws_instance.this
 }
